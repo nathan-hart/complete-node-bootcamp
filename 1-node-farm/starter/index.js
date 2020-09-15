58c21894a32eef2458c5505313a165bace139067
Natan Hart <natan.h@imagesatisrael.com>
2020-09-15 21:51:59 +0300

    completed HTML templates

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -36,15 +36,78 @@ console.log("will read file");  // gets executed first
 
 const fs = require("fs");
 const http = require("http");
+const { report } = require("process");
+const url = require("url");
+
+// SERVER
+// Code that is executed once at Server Setup can be SYNC
+const replaceTemplate = (temp, product) => {
+	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+	
+	if(product.organic) {
+		output = output.replace(/{%NOT_ORGANIC%}/g, '.not-organic');
+	}
+	return output;
+}
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   // createServer module fires a callback function
   //  with params REQUEST and RESPONSE
-  console.log(req);
-  res.end("Hello from server!");
+  const pathName = req.url;
+
+  // Overview Page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+		const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    res.end(output);
+    // Product Page
+  } else if (pathName === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(tempProduct);
+    // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+    // 404 Not Found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>This Page not found</h1>");
+  }
 });
 
 // listen takes in params PORT and HOST (usually localhost)
-server.listen(3000, "127.0.0.1", () => {
-  console.log("listening on port 3000");
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening on port 8000");
 });
