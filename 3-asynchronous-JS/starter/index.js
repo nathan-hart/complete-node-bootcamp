466e84ef4f055d397c164177eead07fe66bf817c
Natan Hart <natan.h@imagesatisrael.com>
2020-09-22 12:55:02 +0300

    async/await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,28 +1,54 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-// Callback HELL!!!!!
+// This function resolves a promise!
+// Promises solution:
 
-// First function
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed name: ${data}`);
-  // Second function - nested
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(res.body.message);
-      // Third function - even more nested
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) {
-          console.log(err);
-        
-          return;
-        }
-        console.log("Random dog image save to file!")
-      });
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I couldnt find file');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('could not write file');
+      resolve('success');
+    });
+  });
+};
+/* 
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed name: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message).then(() => {
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  */
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed name: ${data}`);
+
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog saved to file!!!');
+  } catch (err) {
+    console.log(err);
+  }
+};
+getDogPic();
