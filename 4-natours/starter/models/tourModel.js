a126c406306bec13b4467b25a5c8a0118f42c962
Natan Hart <natan.h@imagesatintl.com>
2020-10-05 14:10:50 +0300

    Start section 9

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have <= 40 chars'],
+      minlength: [5, 'A tour name must have >= 5 chars'],
     },
     slug: String,
     duration: {
@@ -21,11 +24,21 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty level'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Has to be one of three vals',
+      },
     },
-    ratingsQuantity: { type: Number, default: 0 },
+    ratingsQuantity: { type: Number, default: 0, min: [1], max: [5] },
     ratingsAverage: { type: Number, default: 4.3 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: function (val) {
+        // This only points to document on NEW doc creation, not update
+        return val < this.price;
+      },
+    },
     summary: {
       type: String,
       trim: true,
@@ -62,11 +75,29 @@ tourSchema.virtual('durationWeeks').get(function () {
 
 //  Document middleware, runs before .save() and .create()
 tourSchema.pre('save', function (next) {
+  // this keyword points to document
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-tourSchema.pre('find', function (next) {
+//Query Middleware - this happens before query is carried out (has a pre tag)
+tourSchema.pre(/^find/, function (next) {
+  // this keyword points to query object
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query Took ${Date.now() - this.start} ms`);
+  // console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE - this points to current aggregation object
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
